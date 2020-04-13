b7031a8911ac30253a7e6fd8d4ae73807046e16e
Sergey Mironchuk <sergey@mironchuk.in.ua>
2020-04-13 17:21:39 +0300

    Add new Panel component

---
+++ src/components/Forms/MspPanel.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="panel-1" class="panel">
+        <div class="panel-hdr">
+            <h2>
+                {{caption}}
+            </h2>
+            <div class="panel-toolbar">
+                <button class="btn btn-panel" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
+                <button class="btn btn-panel" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
+                <button class="btn btn-panel" data-action="panel-close" data-toggle="tooltip" data-offset="0,10" data-original-title="Close"></button>
+            </div>
+        </div>
+        <div class="panel-container show">
+            <div class="panel-content">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MspPanel",
+        props: {
+            caption: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
